beded8da48e91413d57583ea4911b1346cc8740a
Rafa <rafael.ortega.caceres@hotmail.com>
2021-09-29 15:11:04 +0200

    Add files via upload

--- api.js
+++ api.js
@@ -19,6 +19,6 @@ app.get('/players', playersProvider.getPlayers);
 app.delete('/player/:id', playersProvider.deletePlayer);
 app.put('/player/:id', playersProvider.updateStastics);
 
-mongoose.connect(`mongodb://rafa:rafa1234@ds329668.mlab.com:29668/heroku_nk1dh51w`,
+mongoose.connect(``,
     { useNewUrlParser: true });
 app.listen(process.env.PORT || 5000, () => { console.log('Server listening on port 3000') });
