7e358e753946aa09d873bb5c0411959632cbb112
Rafa <rafael.ortega.caceres@hotmail.com>
2022-11-08 20:50:09 +0100

    Update api.js

--- api.js
+++ api.js
@@ -8,6 +8,8 @@ const competitionProvider = require('./providers/competitions');
 const calendarProvider = require('./providers/calendars');
 const { password } = require('./middleware/password')
 
+require('dotenv').config()
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -26,6 +28,5 @@ app.get('/players', playersProvider.getPlayers);
 app.delete('/player/:id', playersProvider.deletePlayer);
 app.put('/player/:id', password ,playersProvider.updateStastics);
 
-mongoose.connect(`mongodb+srv://orangutan:orangutanclan@cluster-nk1dh51w.ojp1x.mongodb.net/heroku_nk1dh51w?retryWrites=true&w=majority`,
-    { useNewUrlParser: true });
-app.listen(process.env.PORT || 5001, () => { console.log('Server listening on port 5000') });
+mongoose.connect(process.env.MONGO_URI);
+app.listen(process.env.PORT || 5000, () => { console.log('Server listening on port 5000') });
