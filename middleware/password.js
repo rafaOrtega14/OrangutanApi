a32c83842eec235743dd4776122827317419645f
Rafa <rafael.ortega.caceres@hotmail.com>
2022-11-10 18:34:41 +0100

    update pass

--- middleware/password.js
+++ middleware/password.js
@@ -2,7 +2,7 @@ function password(req, res, next) {
     if(req.query.password === process.env.ADMIN_PASS) {
         next()
     } else {
-        res.send('Error password not found' + req.query.password + process.env.ADMIN_PASS)
+        res.send('Error password not found asdasdfasdf' + req.query.password + process.env.ADMIN_PASS)
     }
 }
 
